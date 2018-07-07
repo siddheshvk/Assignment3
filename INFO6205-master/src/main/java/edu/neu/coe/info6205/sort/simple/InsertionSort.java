c2434c2a24ead8ddb1415742988ad300662bbb82
Siddhesh Kuvalekar <sid.kuvalekar@gmail.com>
2018-07-06 22:50:54 -0400

    commit2

--- INFO6205-master/src/main/java/edu/neu/coe/info6205/sort/simple/InsertionSort.java
+++ INFO6205-master/src/main/java/edu/neu/coe/info6205/sort/simple/InsertionSort.java
@@ -6,5 +6,10 @@ public class InsertionSort<X extends Comparable<X>> implements Sort<X> {
     @Override
     public void sort(X[] xs, int from, int to) {
         // TODO implement insertionSort
+        for (int i = 1; i < to; i++)
+        { // Insert a[i] among a[i-1], a[i-2], a[i-3]... ..
+            for (int j = i; j > 0 && less(xs[j], xs[j-1]); j--)
+            swap(xs, j, j-1);
+        }
     }
 }
